c232c1899fac0373dc5aa9d0615defefc8619b47
marlarush <mr106@iu.edu>
2025-11-05 18:56:16 -0500

    Create Question.java

---
+++ src/main/java/edu/solo/personalityquiz/src/main/java/edu/solo/personalityquiz/src/main/java/src/main/java/edu/solo/personalityquiz/Question.java
@@ -0,0 +1,12 @@
+package edu.solo.personalityquiz;
+
+/*
+ * Team: Solo Project – Marla Rush
+ * Contributor: Marla Rush
+ * File: Question.java
+ * Purpose: Represents one quiz question and its multiple-choice options (to be implemented in Phase 2).
+ */
+
+public class Question {
+    // Question text and answer choices will be defined here in Phase 2
+}

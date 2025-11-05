292b6a097ca0b7cfbbfe0c9a07ae08f02a7c938c
marlarush <mr106@iu.edu>
2025-11-05 18:54:58 -0500

    Create Quiz.java

---
+++ src/main/java/edu/solo/personalityquiz/src/main/java/edu/solo/personalityquiz/src/main/java/edu/solo/personalityquiz/Quiz.java
@@ -0,0 +1,12 @@
+package edu.solo.personalityquiz;
+
+/*
+ * Team: Solo Project – Marla Rush
+ * Contributor: Marla Rush
+ * File: Quiz.java
+ * Purpose: Quiz logic: store questions, track answers, compute results (to be implemented in Phase 2).
+ */
+
+public class Quiz {
+    // Quiz logic will go here in Phase 2
+}

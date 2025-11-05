631a81014d340f9eb5ef9e7856cdefe723fc1f7a
marlarush <mr106@iu.edu>
2025-11-05 18:53:46 -0500

    Create QuizFrame.java

---
+++ src/main/java/edu/solo/personalityquiz/src/main/java/edu/solo/personalityquiz/QuizFrame.java
@@ -0,0 +1,12 @@
+package edu.solo.personalityquiz;
+
+/*
+ * Team: Solo Project – Marla Rush
+ * Contributor: Marla Rush
+ * File: QuizFrame.java
+ * Purpose: Swing GUI for displaying quiz questions and results (to be implemented in Phase 2).
+ */
+
+public class QuizFrame {
+    // GUI components and layout will be added in Phase 2
+}

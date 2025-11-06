5b68dcc4f5dbd1b74391f108b5f6113eede05da7
marlarush <mr106@iu.edu>
2025-11-05 19:02:47 -0500

    Create Option.java

---
+++ src/main/java/edu/solo/personalityquiz/src/main/java/edu/solo/personalityquiz/src/main/java/src/main/java/edu/solo/personalityquiz/src/main/java/edu/solo/personalityquiz/Option.java
@@ -0,0 +1,12 @@
+package edu.solo.personalityquiz;
+
+/*
+ * Team: Solo Project – Marla Rush
+ * Contributor: Marla Rush
+ * File: Option.java
+ * Purpose: Represents a single multiple-choice answer and its related personality trait (to be implemented in Phase 2).
+ */
+
+public class Option {
+    // Option details will go here in Phase 2
+}

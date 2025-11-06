fdccedbd0840c661895db944143b5bbed08951b2
marlarush <mr106@iu.edu>
2025-11-05 19:04:00 -0500

    Create Trait.java

---
+++ src/main/java/edu/solo/personalityquiz/src/main/java/edu/solo/personalityquiz/src/main/java/src/main/java/edu/solo/personalityquiz/src/main/java/edu/solo/personalityquiz/src/main/java/edu/solo/personalityquiz/Trait.java
@@ -0,0 +1,13 @@
+package edu.solo.personalityquiz;
+
+/*
+ * Team: Solo Project – Marla Rush
+ * Contributor: Marla Rush
+ * File: Trait.java
+ * Purpose: Enum defining personality traits used by the quiz.
+ */
+
+public enum Trait {
+    INTROVERT,
+    EXTROVERT
+}
